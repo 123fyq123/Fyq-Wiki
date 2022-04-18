5eee114376ebaae9ec5352a62a32359c73a46474
123fyq123 <1210915690@qq.com>
2022-04-18 13:23:27 +0800

    material

---
+++ docs/js/mathjax.js
@@ -0,0 +1,53 @@
+window.MathJax = {
+    tex: {
+        inlineMath: [["\\(", "\\)"]],
+        displayMath: [["\\[", "\\]"]],
+        processEscapes: true,
+        processEnvironments: true
+    },
+    options: {
+        ignoreHtmlClass: ".*|",
+        processHtmlClass: "arithmatex"
+    }
+};
+
+document$.subscribe(() => {
+    MathJax.typesetPromise()
+})
+
+window.MathJax = {
+    loader: { load: ['[tex]/color'] },
+    tex: { packages: { '[+]': ['color'] } }
+};
+
+window.MathJax = {
+    loader: { load: ['[tex]/boldsymbol'] },
+    tex: { packages: { '[+]': ['boldsymbol'] } }
+};
+
+window.MathJax = {
+    tex: {
+        macros: {
+            RR: "{\\bf R}",
+            bold: ["{\\bf #1}", 1]
+        },
+        environments: {
+            braced: ["\\left\\{", "\\right\\}"]
+        }
+    }
+};
+
+window.MathJax = {
+    loader: { load: ['[tex]/configmacros'] },
+    tex: { packages: { '[+]': ['configmacros'] } }
+};
+
+window.MathJax = {
+    loader: { load: ['[tex]/mathtools'] },
+    tex: { packages: { '[+]': ['mathtools'] } }
+};
+
+window.MathJax = {
+    loader: { load: ['[tex]/textmacros'] },
+    tex: { packages: { '[+]': ['textmacros'] } }
+};
